56a6f1c52bbdf0cf202dab3db1b4a9a10789a329
RomainC75 <rom.chenard@gmail.com>
2024-03-06 21:05:56 +0100

    get a range of addresses

--- main.go
+++ main.go
@@ -9,16 +9,23 @@ import (
 
 // const target1 = "google.com"
 const target_min = "212.0.2.43"
-const target_max = "212.23.0.45"
+const target_max = "212.0.3.43"
 
 func main() {
 	// for {
 	// 	time.Sleep(time.Second * 1)
 	// 	ping.Ping(target)
 	// }
-	res, err := utils.ParseAdress(target_max)
+	min, err := utils.ParseAdress(target_min)
 	if err != nil {
 		log.Fatalf("error :  %s\n", err.Error())
 	}
+	max, err := utils.ParseAdress(target_max)
+	if err != nil {
+		log.Fatalf("error :  %s\n", err.Error())
+	}
+	res := utils.GetAddresses(min, max)
+
 	fmt.Println(res)
+	fmt.Println("len : ", len(res))
 }
--- utils/getRange.go
+++ utils/getRange.go
@@ -1,17 +1,21 @@
 package utils
 
-import "sync"
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
 
-type Addresses struct {
-	addresses []string
-	m         sync.Mutex
-}
+// type Addresses struct {
+// 	addresses []string
+// 	m         sync.Mutex
+// }
 
-func New() *Addresses {
-	return &Addresses{
-		addresses: []string{},
-	}
-}
+// func New() *Addresses {
+// 	return &Addresses{
+// 		addresses: []string{},
+// 	}
+// }
 
 // func (a *Addresses) GetEveryAddresses(min []int, max []int) []int {
 // 	for
@@ -28,9 +32,46 @@ func IsMinBeforeMax(min []int, max []int) bool {
 	return true
 }
 
-// func GetAddresses(min string, max string) []int {
+func GetAddresses(min []int, max []int) []string {
+	res := []string{}
+	for IsMinBeforeMax(min, max) {
+		strAddress, err := getStringAddress(min)
+		if err == nil {
+			res = append(res, strAddress)
+		}
+		increment(min, max)
+	}
+	return res
+}
 
-// }
+func increment(min []int, max []int) {
+	isMax := true
+	index := 3
+	for isMax && index >= 0 {
+		if min[index] == 255 {
+			min[index] = 0
+		} else {
+			min[index]++
+			isMax = false
+		}
+		index--
+	}
+}
+
+func getStringAddress(addr []int) (string, error) {
+	strAddr := []string{}
+	if len(addr) != 4 {
+		return "", errors.New("wrong length")
+	}
+	for _, intByte := range addr {
+		if intByte < 0 || intByte > 255 {
+			return "", errors.New("numbers should be between 0 and 255")
+		}
+		strByte := strconv.Itoa(intByte)
+		strAddr = append(strAddr, strByte)
+	}
+	return strings.Join(strAddr, "."), nil
+}
 
 // func GetIntAddresses(min []int, max []int) []int {
 // 	for i, val := range max {
